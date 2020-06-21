6b8c21788bc01924be4a44eee8500f38a2682a64
Joel Davis <joeladavisdev@gmail.com>
2020-06-20 21:20:14 -0600

    navigation

--- front-end/src/components/app.js
+++ front-end/src/components/app.js
@@ -1,12 +1,29 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-export default class App extends Component {
-  render() {
-    return (
-      <div className="app">
-        <h1>DevCamp React Starter</h1>
-        <h2>React + React Router</h2>
-      </div>
-    );
-  }
+import NavigationContainer from '../navigation/navigation-container'
+import Home from './home';
+import CodeChallenges from './code-challenges';
+import ToughQuestions from './tough-questions';
+import AppearanceTips from './appearance-tips';
+import ProspectiveCompanies from './prospective-companies';
+
+export default function App() {
+  
+  return (
+    <Router>
+    <NavigationContainer />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/code-challenges" component={CodeChallenges} />
+        <Route path="/tough-questions" component={ToughQuestions} />
+        <Route path="/appearance-tips" component={AppearanceTips} />
+        <Route path="/prospective-companies" component={ProspectiveCompanies} />
+      </Switch>
+      
+
+    </Router>
+    
+  );
+  
 }
---
+++ front-end/src/components/tough-questions.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class ToughQuestions extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Tough Questions</h1>
+            </div>
+        )
+    }
+}
+
+export default ToughQuestions
